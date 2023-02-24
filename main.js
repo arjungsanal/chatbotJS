9d1521b1c218918bd756f38f1bef875110191e97
arjunindia <arjun8107@gmail.com>
2023-02-24 23:25:22 +0530

    TS

--- main.js
+++ main.js
@@ -1,35 +1,32 @@
-const text = document.querySelector('textarea');
-const button = document.querySelector('button');
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var text = document.querySelector("textarea");
+var button = document.querySelector("button");
 text.focus();
-
-button.addEventListener('click', (e) => {
+button.addEventListener("click", function (e) {
     e.preventDefault();
-    if(!(text.value.trim() === '')) {
-    const message = text.value.trim();   
-    const msg = document.createElement('div');
-    msg.classList.add('sent');
-    msg.textContent = text.value;
-    document.querySelector('.inputgroup').before(msg);
-    text.value = '';
-
-    fetch(`https://corsproxy.io/?http://api.brainshop.ai/get?bid=172002&key=WzDHnNNnSFl5HQRc&uid=[uid]&msg=${message}`)
-        .then(response => response.json())
-        .then(data => {
-            const p = document.createElement('p');
+    if (!(text.value.trim() === "")) {
+        var message = text.value.trim();
+        var msg = document.createElement("div");
+        msg.classList.add("sent");
+        msg.textContent = text.value;
+        document.querySelector(".inputgroup").before(msg);
+        text.value = "";
+        fetch("https://corsproxy.io/?http://api.brainshop.ai/get?bid=172002&key=WzDHnNNnSFl5HQRc&uid=[uid]&msg=".concat(message))
+            .then(function (response) { return response.json(); })
+            .then(function (data) {
+            var p = document.createElement("p");
             p.textContent = data.cnt;
-            const div = document.createElement('div');
-            div.classList.add('text');
+            var div = document.createElement("div");
+            div.classList.add("text");
             div.appendChild(p);
-            document.querySelector('.inputgroup').before(div);
-    document.querySelector('.inputgroup').scrollIntoView();
-
-        }).catch(err => {
+            document.querySelector(".inputgroup").before(div);
+            document.querySelector(".inputgroup").scrollIntoView();
+        })
+            .catch(function (err) {
             console.log(err);
         });
-        
-    document.querySelector('.inputgroup').scrollIntoView();
-    text.focus();
+        document.querySelector(".inputgroup").scrollIntoView();
+        text.focus();
     }
-    }
-);
+});
